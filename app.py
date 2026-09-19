d528ed88c921f1f858a01cfc6acea11772c7294f
luciano-dsg <138631651+luciano-dsg@users.noreply.github.com>
2026-09-18 21:41:52 -0300

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import streamlit as st
 
 st.header('Análise de anúncios de venda de carros')
 car_data = pd.read_csv('vehicles.csv') # lendo os dados
-hist_button = st.button('Criar histograma') # criar um botão
+hist_button = st.checkbox('Criar histograma') # criar um botão
 if hist_button: # se o botão for clicado
     st.write('Criando um histograma para o conjunto de dados de anúncios de vendas de carros') # escrever uma mensagem
 
@@ -14,7 +14,7 @@ if hist_button: # se o botão for clicado
     # exibir um gráfico Plotly interativo
     st.plotly_chart(fig, use_container_width=True)
 
-scatter_button = st.button('Criar gráfico de dispersão')
+scatter_button = st.checkbox('Criar gráfico de dispersão')
 
 if scatter_button:
     st.write('Criando um gráfico de dispersão entre quilometragem e preço')
